7aa49dae2be9cadd76813b8a1e5c1b8f018f6dc0
Badr-1 <badrmohamedashraf@gmail.com>
2023-09-14 14:17:40 +0300

    ➕ Add cli and console styling dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,19 @@ repositories {
     mavenCentral()
 }
 
+allprojects {
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.fusesource.jansi:jansi:2.4.0")
+    implementation ("org.jline:jline:3.21.0")
+    implementation("com.github.ajalt.clikt:clikt:4.2.0")
+    implementation("com.github.ajalt.mordant:mordant:2.1.0")
+    implementation("com.github.kotlin-inquirer:kotlin-inquirer:0.1.0")
 }
 
 tasks.test {
